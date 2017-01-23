f9d6281b1944fb152ae2e413f9be9a9278e518d4
Erin Casteel <erin@erincasteel.com>
2017-01-23 14:15:21 -0800

    Fix google url protocols + image gallery paths; style + animate home page more link

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+    var $root = $('html, body');
+
+    setTimeout(function () {
+        $('.more-link').show().addClass('animated bounceInDown');}, 2000
+    );
+
+    $('.more-link').on('click', function(event){     
+        event.preventDefault();
+        $root.animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
 
 	$('#photo-gallery').justifiedGallery({
 		'rowHeight': 300,
