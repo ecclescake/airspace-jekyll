e52a2f47ed4c6d974d4fee794dac5263e4859501
Erin Casteel <erin@erincasteel.com>
2017-01-23 21:23:19 -0800

    Add jumplink to location section; hide more link at small screen sizes

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 
     var $root = $('html, body');
 
-    setTimeout(function () {
-        $('.more-link').show().addClass('animated bounceInDown');}, 2000
-    );
+    if($(window).width() >= 768){
+        setTimeout(function () {
+            $('.delayed').show().addClass('animated bounceInDown');}, 2000
+        );
+    }
 
     $('.more-link').on('click', function(event){     
         event.preventDefault();
