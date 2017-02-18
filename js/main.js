1ee471fb44faa2ac4ce0d2f57255eddbc84c6150
Erin Casteel <erin@erincasteel.com>
2017-02-17 17:23:10 -0800

    Attempt to fix home hero image on mobile

--- js/main.js
+++ js/main.js
@@ -13,14 +13,14 @@ $(document).ready(function(){
         $root.animate({scrollTop:$(this.hash).offset().top}, 500);
     });
 
-	$('#photo-gallery').justifiedGallery({
-		'rowHeight': 300,
+    $('#photo-gallery').justifiedGallery({
+        'rowHeight': 300,
         'lastRow': 'hide'
-	});
+    });
 
+    var ismobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    if (ismobile){
+     $('#slider').css('background-attachment', 'scroll');
+    }
 
 });
-
-
-
-
